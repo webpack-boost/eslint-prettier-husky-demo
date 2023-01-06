7cc9a56415abe3dd7eb71212012fb536fabf0398
maomiao <maomiao@mrray.cn>
2023-01-06 14:47:15 +0800

    feat(custom): vue-eslint基础配置项

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,23 @@ module.exports = {
     commonjs: true,
     amd: true
   },
-  extends: ['airbnb-base', 'eslint:recommended', 'prettier'],
-  plugins: ['prettier', 'json', 'html', 'markdown'],
-  overrides: [
-    {
-      // 2. Enable the Markdown processor for all .md files.
-      files: ['**/*.md'],
-      processor: 'markdown/markdown'
-    }
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     parser: 'babel-eslint'
   },
+  /*
+   * "off" 或 0    ==>  关闭规则
+   * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
+   * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
+   */
   rules: {
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'development' ? 'off' : 'error',
